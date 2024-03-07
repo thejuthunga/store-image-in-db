d78b009c1c70b028b09432975bb04c0a60a107ae
thejuthunga <thejasthejashree@gmail.com>
2024-03-07 10:51:54 +0530

    modified image controller

--- src/main/java/com/ff/spring/Imagestoringindb/controller/ImageController.java
+++ src/main/java/com/ff/spring/Imagestoringindb/controller/ImageController.java
@@ -1,14 +1,10 @@
 package com.ff.spring.Imagestoringindb.controller;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -42,43 +38,12 @@ public class ImageController {
 		}
 		return null;
 	}
-
-	
 	@GetMapping("/download/{id}")
-	public ResponseEntity<?> getImageById(@PathVariable int id){
+	public ResponseEntity<Image> getImageById(@PathVariable int id){
 		 Image image = service.downloadImage(id);
 	        if (image == null) {
 	            return ResponseEntity.notFound().build();
 	        }
-	        // Load image from file system or blob storage based on the path
-	        File file = new File(image.getPath());
-	        if (!file.exists()) {
-	            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Image not found on server");
-	        }
-	        try {
-	            byte[] bytes = Files.readAllBytes(file.toPath());
-	            HttpHeaders headers = new HttpHeaders();
-	            headers.setContentType(MediaType.IMAGE_JPEG); // Adjust content type based on your image type
-	            headers.setContentLength(bytes.length);
-	            return new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
-	        } catch (IOException e) {
-	            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reading image file");
-	        }
-	}
-	
-	
-	
-	
-	
-	/*
-	@GetMapping("/download/{id}")
-	public ResponseEntity<String> downloadImage(@PathVariable int id,HttpServletResponse response) throws IOException{
-		InputStream  resource=service.downloadImage(path, id);
-		
-		response.setContentType(MediaType.IMAGE_PNG_VALUE);
-		
-		StreamUtils.copy(resource, response.getOutputStream());
-		
-		
-	} */
+	            return new ResponseEntity<Image>(image,HttpStatus.OK);  
+	}	
 }
---
+++ src/main/java/com/ff/spring/Imagestoringindb/dto/ImageDto.java
@@ -0,0 +1,11 @@
+package com.ff.spring.Imagestoringindb.dto;
+
+import lombok.Data;
+
+@Data
+public class ImageDto {
+	
+	private int id;
+	private String name;
+	private String path;
+}
